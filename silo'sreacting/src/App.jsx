73729699a3817283a71e8489d875e33edfbaf532
SaleenaAhuja20 <saleenaahuja038@gmail.com>
2025-07-15 19:52:24 +0500

    Update App.jsx

--- silo'sreacting/src/App.jsx
+++ silo'sreacting/src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  var [a,b] = useState(69);
   return (
     <>
-    <div className='w-full h-screen bg-zinc-500'>
-      <div className='w-32 h-32 bg-red-400 rounded-e-4xl place-content-center gap-<5>'>great learning</div>
+    <div className='w-full h-screen bg-zinc-500 p-4'>
+      <h1>{a}</h1>
+      <button onClick={()=> b(a+1)} className='py-1 px-3 rounded-md bg-green-600 text-white cursor-pointer'>click</button>
+      {/* here b is updator  */}
     </div>
     </>
-  )
+  ) 
 }
 
 export default App 
