887016849798d1b0c4ca586238cfa2253eb09e8e
SaleenaAhuja20 <saleenaahuja038@gmail.com>
2025-07-15 15:41:40 +0500

    .

---
+++ silo'sreacting/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <div className='w-full h-screen bg-zinc-500'>
+      <div className='w-32 h-32 bg-red-400 rounded-e-4xl place-content-center gap-<5>'>great learning</div>
+    </div>
+    </>
+  )
+}
+
+export default App 
